c66cfae74f9c5a0adfc47e712218fc887d2fb976
boumi21 <glorkpatate@yahoo.be>
2018-04-28 23:10:36 +0200

    Entries stored
    Instances of the entries are now stored into 2 differents lists.

--- veryTest2.py
+++ veryTest2.py
@@ -6,6 +6,8 @@ import csv
 policyFile = "firewallpolicies.csv"
 entries1 = []
 entries2 = []
+listEntries1 = []
+listEntries2 = []
 
 
 def createWidgets(root):
@@ -19,6 +21,7 @@ def createWidgets(root):
 		lab = Label(row, text='Rule '+str(counter), anchor='w')
 		ent1 = Entry(row)
 		ent1.insert(0, field)
+		listEntries1.append(ent1)
 		row.pack(side=TOP, fill=X, padx=5, pady=5)
 		lab.pack(side=LEFT)
 		ent1.pack(side=LEFT)
@@ -28,29 +31,29 @@ def createWidgets(root):
 		#lab = Label(row, width=15, text=field, anchor='w')
 		ent2 = Entry(row2)
 		ent2.insert(0, field)
+		listEntries2.append(ent2)
 		row2.pack(side=TOP, fill=X, padx=5, pady=5)
 		ent2.pack(side=RIGHT, pady=1)
-	print ent2.get()
 
 
 
 def getFirewall():
-    ifile  = open(policyFile, "rb")
-    reader = csv.reader(ifile)
-    rownum = 0
-    for row in reader:
-        # Save header row.
-        if rownum == 0:
-            header = row
-        else:
-            colnum = 0
-            for col in row:
-                #print '%-8s: %s' % (header[colnum], col)
-                colnum += 1
-            entries1.append(row[1])
-            entries2.append(row[2])
-        rownum += 1
-    ifile.close()
+		ifile  = open(policyFile, "rb")
+		reader = csv.reader(ifile)
+		rownum = 0
+		for row in reader:
+				# Save header row.
+				if rownum == 0:
+						header = row
+				else:
+						colnum = 0
+						for col in row:
+								#print '%-8s: %s' % (header[colnum], col)
+								colnum += 1
+						entries1.append(row[1])
+						entries2.append(row[2])
+				rownum += 1
+		ifile.close()
 
 
 
@@ -59,12 +62,12 @@ def getEntries():
 
 
 if __name__ == '__main__':
-   root = Tk()
-   root.title('Adninistrator panel')
-   getFirewall()
-   createWidgets(root)
-   buttonSave = Button(root, text="Save", command=getEntries)
-   buttonSave.grid(row=1, column=0)
-   buttonQuit = Button(root, text='Quit', command=root.quit)
-   buttonQuit.grid(row=1, column=1)
-   root.mainloop()
+	 root = Tk()
+	 root.title('Adninistrator panel')
+	 getFirewall()
+	 createWidgets(root)
+	 buttonSave = Button(root, text="Save", command=getEntries)
+	 buttonSave.grid(row=1, column=0)
+	 buttonQuit = Button(root, text='Quit', command=root.quit)
+	 buttonQuit.grid(row=1, column=1)
+	 root.mainloop()
